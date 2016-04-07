8d66a44d12aa2a9cf31be5544801b155b42a23c7
Will Sargent <will.sargent@gmail.com>
2016-04-06 17:20:19 -0700

    Add commments

--- app/myroutes/AppRoutes.scala
+++ app/myroutes/AppRoutes.scala
@@ -8,12 +8,14 @@ import play.api.mvc.Call
 object AppRoutes {
 
   object WebControllerRoutes {
+    //  target/scala-2.10/src_managed/main/web/controllers/routes.scala
     val hello = web.controllers.routes.WebController.hello()
 
-    private val helloRoutes = Seq(
+    private val helloUrls = Seq(
       hello.url
     )
   }
 
+  // target/scala-2.10/src_managed/main/controllers/routes.java
   val index: Call = controllers.routes.HomeController.index()
 }
